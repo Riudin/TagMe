c5cf291d987e240f104f613879b1666be342cdf1
Christoph Sengestack <christoph@sengestack.de>
2023-11-04 14:36:27 +0100

    imported packages

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
-print("Hello World!")
+# import libraries
+import pandas as pd                 # dataframe to visualize data (mainly for testing and confirming if the code works)
+import numpy as np                  # fundamental package for scientific computing. includes advanced mathematical methods like n-dimensional arrays and vectorization
+import matplotlib.pyplot as plt     # package for plotting data as graphs
